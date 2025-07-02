c090268fe205ecee9da21ed39f37afbe23e4f36c
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2025-07-02 19:01:55 +0530

    production testing

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,3 @@
 export const PORT = 8000;
-export const BASE_URL = `http://localhost:${PORT}`;
+// export const BASE_URL = `http://localhost:${PORT}`;
+export const BASE_URL = `https://nowcast-ai-backend.onrender.com`;
